23d0e4bdf086b959a13414bb043a525ce372d4fa
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-07-27 11:23:49 +0900

    :gear: :: Spring initializer

---
+++ src/main/java/jhhong/example/rsocketchatting/RsocketChattingApplication.java
@@ -0,0 +1,13 @@
+package jhhong.example.rsocketchatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketChattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketChattingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jhhong/example/rsocketchatting/RsocketChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package jhhong.example.rsocketchatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
