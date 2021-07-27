2395649fe5a6f0d5b0a8c82815b6157abbe81fdc
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-07-27 14:29:46 +0900

    ⚡️ :: UserRepository와 User Entity 추가

--- src/main/java/jhhong/example/rsocketchatting/domain/user/entity/User.java
+++ src/main/java/jhhong/example/rsocketchatting/domain/user/entity/User.java
@@ -1,2 +1,24 @@
-package jhhong.example.rsocketchatting.domain.user.entity;public class User {
+package jhhong.example.rsocketchatting.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@Document
+public class User {
+
+    @MongoId
+    private final String email;
+
+    @NonNull
+    private final String password;
+
+    @NonNull
+    private final String nickname;
 }
--- src/main/java/jhhong/example/rsocketchatting/domain/user/entity/UserRepository.java
+++ src/main/java/jhhong/example/rsocketchatting/domain/user/entity/UserRepository.java
@@ -1,2 +1,6 @@
-package jhhong.example.rsocketchatting.domain.user.entity;public interface UserREpository {
+package jhhong.example.rsocketchatting.domain.user.entity;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
 }
