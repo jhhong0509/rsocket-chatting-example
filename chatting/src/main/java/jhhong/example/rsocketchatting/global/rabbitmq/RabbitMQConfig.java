8b5abef6b42f1c78cc21ebb7bcf684acc8154c88
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-09 10:14:01 +0900

    ⚡️ :: RabbitMQ 설정 추가

--- chatting/src/main/java/jhhong/example/rsocketchatting/global/rabbitmq/RabbitMQConfig.java
+++ chatting/src/main/java/jhhong/example/rsocketchatting/global/rabbitmq/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package jhhong.example.rsocketchatting.global.rabbitmq;
 
-import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,20 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     @Bean
-    public FanoutExchange fanoutExchange() {
-        return new FanoutExchange("pl.kubaretip.chatmessagesservice.fanout");
+    public Queue chatRoomQueue() {
+        return new Queue("topic.room", true);
+    }
+
+    @Bean
+    public Binding bindingTopicExchange() {
+        return BindingBuilder.bind(topicExchange())
+                .to(topicExchange())
+                .with("topic.public.*");
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange("chatroom-id");
     }
 
 }
