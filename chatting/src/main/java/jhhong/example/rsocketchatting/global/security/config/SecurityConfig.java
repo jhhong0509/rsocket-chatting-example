6e8b68aa0d743a0e6e1f8e631b1fa15b18860700
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-21 11:38:53 +0900

    ⚡️ :: Bean 등록 한번만 하거나 안하게 수정

--- chatting/src/main/java/jhhong/example/rsocketchatting/global/security/config/SecurityConfig.java
+++ chatting/src/main/java/jhhong/example/rsocketchatting/global/security/config/SecurityConfig.java
@@ -2,19 +2,29 @@ package jhhong.example.rsocketchatting.global.security.config;
 
 import jhhong.example.rsocketchatting.global.adapter.outbound.UserAdapter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.rsocket.RSocketStrategies;
-import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
 import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.messaging.handler.invocation.reactive.AuthenticationPrincipalArgumentResolver;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtReactiveAuthenticationManager;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.rsocket.authentication.BearerPayloadExchangeConverter;
+import org.springframework.security.rsocket.authentication.PayloadExchangeAuthenticationConverter;
 import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor;
+import reactor.core.Disposables;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @RequiredArgsConstructor
 @Configuration
@@ -23,7 +33,26 @@ import org.springframework.security.rsocket.core.PayloadSocketAcceptorIntercepto
 public class SecurityConfig {
 
     private final UserAdapter userAdapter;
-    private final JwtReactiveAuthenticationManager jwtReactiveAuthenticationManager;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Bean
+    public PayloadExchangeAuthenticationConverter authenticationConverter() {
+        return new BearerPayloadExchangeConverter();
+    }
+
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(), MacAlgorithm.HS256.getName());
+
+        return NimbusReactiveJwtDecoder.withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+
+    public JwtReactiveAuthenticationManager jwtReactiveAuthenticationManager() {
+        return new JwtReactiveAuthenticationManager(reactiveJwtDecoder());
+    }
 
     @Bean
     PayloadSocketAcceptorInterceptor authorization(RSocketSecurity security) {      // Jwt를 인증하고 권한을 결정한다.
@@ -31,13 +60,8 @@ public class SecurityConfig {
                         authorize
                                 .setup().permitAll()
                                 .anyExchange().authenticated())
-                .jwt(jwt -> {
-                    try {
-                        jwt.authenticationManager(jwtReactiveAuthenticationManager);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }).build();
+                .jwt(jwtSpec -> jwtSpec.authenticationManager(jwtReactiveAuthenticationManager()))
+                .build();
     }
 
     @Bean
