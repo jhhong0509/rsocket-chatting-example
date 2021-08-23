6914c5689394c5f4c2f83d97707fb4ca954f241e
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-23 12:53:04 +0900

    ⚡️ :: 현재 유저의 sub를 반환하는 어노테이션 ㅅ추가

---
+++ chatting/src/main/java/jhhong/example/rsocketchatting/global/security/annotation/CurrentUser.java
@@ -0,0 +1,14 @@
+package jhhong.example.rsocketchatting.global.security.annotation;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "getClaims().get('sub')")
+public @interface CurrentUser {
+}
